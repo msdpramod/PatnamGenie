6d848695d00db045803f96912f3759e020499f84
msdpramod <kuppapramod@gmail.com>
2024-01-31 22:25:35 +0530

    hi patnam genie

---
+++ src/main/java/org/commerceproject/patnamgeniesaloonservice/PatnamGenieSaloonServiceApplication.java
@@ -0,0 +1,13 @@
+package org.commerceproject.patnamgeniesaloonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatnamGenieSaloonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatnamGenieSaloonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/commerceproject/patnamgeniesaloonservice/PatnamGenieSaloonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commerceproject.patnamgeniesaloonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatnamGenieSaloonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
